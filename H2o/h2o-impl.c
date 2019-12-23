1fffa3724e63dc6e4ba23789707cf9716c410d8b
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2019-12-22 22:01:22 -0300

    Read waits for hydrogen to be written v1.0.10.5

--- H2o/h2o-impl.c
+++ H2o/h2o-impl.c
@@ -1,38 +1,38 @@
-///
-/// Linux module to produce molecules of H2O.
-///
-/// An H2O molecule needs 2 hydrogen particles and 1 particle of oxygen.
-/// - Hydrogen particles are provided by performing a write operation on ``/dev/h2o``.
-/// - Oxygen particles are provided by performing a read operation on ``/dev/h2o``.
-///
-/// Both writing and reading tasks must wait until a molecule is created to finish, but a
-/// read operation can be interrupted with the ``<Control+C>`` signal.
-///
-/// When the molecule is created, the read operation returns the concatenation of the
-/// parameters given to the write command in FIFO order.
-///
-/// Example usage:
-/// | Shell 1                   | Shell 2                   | Shell 3                   | Shell 4              |
-/// | ------------------------- | ------------------------- | ------------------------- | -------------------- |
-/// | ``$ echo abc > /dev/h2o`` |                           |                           |                      |
-/// |                           | ``$ echo def > /dev/h2o`` |                           |                      |
-/// |                           |                           | ``$ echo ghi > /dev/h2o`` |                      |
-/// |                           |                           |                           | ``$ cat < /dev/h20`` |
-/// | ``$``                     | ``$``                     |                           | ``abc``              |
-/// |                           |                           |                           | ``ghi``              |
-/// |                           | ``$ echo jkl > /dev/h2o`` |                           |                      |
-/// |                           |                           |                           |                      |
-/// | ``$ echo mno > /dev/h2o`` |                           |                           |                      |
-/// |                           |                           | ``$ echo pqr > /dev/h20`` |                      |
-/// | ``$``                     |                           | ``$``                     | ``mno``              |
-/// |                           |                           |                           | ``pqr``              |
-/// |                           |                           |                           | ``<Control+C>``      |
-/// |                           |                           |                           | ``$``                |
-///
-/// \author Ignacio Slater Muñoz
-/// \version 1.0.10.4
-/// \since 1.0
-
+/**
+* Linux module to produce molecules of H2O.
+*
+* An H2O molecule needs 2 hydrogen particles and 1 particle of oxygen.
+* - Hydrogen particles are provided by performing a write operation on ``/dev/h2o``.
+* - Oxygen particles are provided by performing a read operation on ``/dev/h2o``.
+*
+* Both writing and reading tasks must wait until a molecule is created to finish, but a
+* read operation can be interrupted with the ``<Control+C>`` signal.
+*
+* When the molecule is created, the read operation returns the concatenation of the
+* parameters given to the write command in FIFO order.
+*
+* Example usage:
+* | Shell 1                   | Shell 2                   | Shell 3                   | Shell 4              |
+* | ------------------------- | ------------------------- | ------------------------- | -------------------- |
+* | ``$ echo abc > /dev/h2o`` |                           |                           |                      |
+* |                           | ``$ echo def > /dev/h2o`` |                           |                      |
+* |                           |                           | ``$ echo ghi > /dev/h2o`` |                      |
+* |                           |                           |                           | ``$ cat < /dev/h20`` |
+* | ``$``                     | ``$``                     |                           | ``abc``              |
+* |                           |                           |                           | ``ghi``              |
+* |                           | ``$ echo jkl > /dev/h2o`` |                           |                      |
+* |                           |                           |                           |                      |
+* | ``$ echo mno > /dev/h2o`` |                           |                           |                      |
+* |                           |                           | ``$ echo pqr > /dev/h20`` |                      |
+* | ``$``                     |                           | ``$``                     | ``mno``              |
+* |                           |                           |                           | ``pqr``              |
+* |                           |                           |                           | ``<Control+C>``      |
+* |                           |                           |                           | ``$``                |
+*
+* \author Ignacio Slater Muñoz
+* \version 1.0.10.5
+* \since 1.0
+*/
 #pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #pragma GCC diagnostic ignored "-Wunused-label"
 #pragma clang diagnostic push
@@ -59,103 +59,109 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 // region : global declarations
-/// Size of the buffer to store data
+/** Size of the buffer to store data*/
 #define MAX_SIZE 8192
 // endregion
 
 // region : Declaration of h2o.c functions
 
-/// Opens the H2O module.
-/// Each time the module is opened, it's file descriptor is different.
-///
-/// \param inode
-///     a struct containing the characteristics of the file.
-/// \param pFile
-///     the file descriptor.
-/// \returns 0 if the operation was succesfull; an error code otherwise.
+/**
+* Opens the H2O module.
+* Each time the module is opened, it's file descriptor is different.
+*
+* \param inode
+*     a struct containing the characteristics of the file.
+* \param pFile
+*     the file descriptor.
+* \returns 0 if the operation was succesfull; an error code otherwise.
+ */
 static int openH2O(struct inode *inode, struct file *pFile);
 
-/// Closes the H2O module.
-/// Each time the module is opened, it's file descriptor is different.
-///
-/// \param inode
-///     a struct containing the characteristics of the file.
-/// \param pFile
-///     the file descriptor.
-///
-/// \returns 0 if the operation was succesfull; an error code otherwise.
-static int releaseH2O(struct inode *inode, struct file *pFile);
+/**
+* Closes the H2O module.
+* Each time the module is opened, it's file descriptor is different.
+*
+* \param inode
+*     a struct containing the characteristics of the file.
+* \param pFile
+*     the file descriptor.
+*
+* \returns 0 if the operation was succesfull; an error code otherwise.
+*/static int releaseH2O(struct inode *inode, struct file *pFile);
 
-/// Reads a fragment of the file.
-/// If there are bytes remaining to be read from ``pFilePos``, then the function returns
-/// ``count`` and ``pFilePos`` is moved to the first unread byte.
-///
-/// \param pFile
-///     the file descriptor.
-/// \param buf
-///     the direction where the read data should be placed.
-/// \param count
-///     the maximum number of bytes to read.
-/// \param pFilePos
-///     the position from where the bytes should be read.
-///
-/// \returns the number of bytes read, 0 if it reaches the file's end or an error code if
-///     the read operation fails.
-static ssize_t readH2O(struct file *pFile, char *buf, size_t count, loff_t *pFilePos);
+/**
+* Reads a fragment of the file.
+* If there are bytes remaining to be read from ``pFilePos``, then the function returns
+* ``count`` and ``pFilePos`` is moved to the first unread byte.
+*
+* \param pFile
+*     the file descriptor.
+* \param buf
+*     the direction where the read data should be placed.
+* \param count
+*     the maximum number of bytes to read.
+* \param pFilePos
+*     the position from where the bytes should be read.
+*
+* \returns the number of bytes read, 0 if it reaches the file's end or an error code if
+*     the read operation fails.
+*/static ssize_t readH2O(struct file *pFile, char *buf, size_t count, loff_t *pFilePos);
 
-/// Adds a hydrogen particle to the file.
-/// If the number of bytes to be written is larger than the maximum buffer size, then only
-/// the bytes that doesn't exceed the buffer size are written to the file and an error
-/// code is returned.
-///
-/// \param pFile
-///     the file descriptor.
-/// \param buf
-///     the data to be written in the file.
-/// \param count
-///     the maximum number of bytes to write.
-/// \param pFilePos
-///     the position from where the bytes should be written.
-static ssize_t writeH2O(struct file *pFile, const char *buf, size_t count,
-                        loff_t *pFilePos);
+/**
+* Adds a hydrogen particle to the file.
+* If the number of bytes to be written is larger than the maximum buffer size, then only
+* the bytes that doesn't exceed the buffer size are written to the file and an error
+* code is returned.
+*
+* \param pFile
+*     the file descriptor.
+* \param buf
+*     the data to be written in the file.
+* \param count
+*     the maximum number of bytes to write.
+* \param pFilePos
+*     the position from where the bytes should be written.
+*/static ssize_t writeH2O(struct file *pFile, const char *buf, size_t count,
+                          loff_t *pFilePos);
 
-/// Unregisters the H2O driver and releases it's buffer.
-void exitH2O(void);
+/** Unregisters the H2O driver and releases it's buffer.
+*/void exitH2O(void);
 
-/// Registers the H2O driver and initializes it's buffer.
-int initH2O(void);
+/** Registers the H2O driver and initializes it's buffer.
+*/int initH2O(void);
 
 // endregion
 
 // region : Helper functions
+/**
+* Enqueues an atom of hydrogen to form a water molecule.
+* If there's already 2 hydrogens in the queue, this function waits until a molecule is
+* created.
+*
+* \param buf
+*     the data contained in the atom.
+* \return 0 if the operation was successful or an error code otherwise.
+*/static ssize_t enqueueHydrogen(const char *buf);
 
-/// Enqueues an atom of hydrogen to form a water molecule.
-/// If there's already 2 hydrogens in the queue, this function waits until a molecule is
-/// created.
-///
-/// \param buf
-///     the data contained in the atom.
-/// \return 0 if the operation was successful or an error code otherwise.
-static ssize_t enqueueHydrogen(const char *buf);
+/** Ends a writing process and returns a code indicating if it was successful or not.
+*/static void endWrite(int code, const char *buf);
 
-/// Ends a writing process and returns a code indicating if it was successful or not.
-static void endWrite(int code, const char *buf);
-
-/// Writes the bytes of buf into the module and returns a code indicating if it was
-/// successful or not.
-static ssize_t writeBytes(size_t count, const char *buf);
+/**
+* Writes the bytes of buf into the module and returns a code indicating if it was
+* successful or not.
+*/static ssize_t writeBytes(size_t count, const char *buf);
 
 // endregion
 
-/// Structure that declares the usual file access functions.
-struct file_operations pH2OFileOperations = {
+/** Structure that declares the usual file access functions.
+*/struct file_operations pH2OFileOperations = {
     .read = readH2O,
     .write = writeH2O,
     .open = openH2O,
     .release = releaseH2O};
 
-/// Major number
-int h2oMajor = 60;
+/** Major number
+*/int h2oMajor = 60;
 
 // region : local variables
 static char *bufferH2O;
@@ -164,12 +170,12 @@ static const char
     FALSE = 0,
     TRUE = 1;
 
-/// H2O's mutex
-static KMutex mutex;
-/// Mutex conditions
-static KCondition cond, waitingMolecule, waitingHydrogen;
-static int enqueuedHydrogens, oxygens;
-static char formingMolecule;
+/** H2O's mutex
+*/static KMutex mutex;
+/** Mutex conditions
+*/static KCondition cond, waitingMolecule, waitingHydrogen;
+static int enqueuedHydrogens, enqueuedOxygens;
+static int writtenHydrogens;
 // endregion
 
 // region : Declaration of the init and exit functions
@@ -190,10 +196,10 @@ int initH2O(void) {
     printk("<1>h2o: cannot obtain major number %d\n", h2oMajor);
     return returnCode;
   }
-  oxygens = 0;
+  enqueuedOxygens = 0;
   enqueuedHydrogens = 0;
+  writtenHydrogens = 0;
   in = out = size = 0;
-  formingMolecule = FALSE;
   m_init(&mutex);
   c_init(&cond);
   c_init(&waitingMolecule);
@@ -249,8 +255,8 @@ static ssize_t readH2O(struct file *pFile, char *buf, size_t ucount, loff_t *pFi
   m_lock(&mutex);
   printk("DEBUG:readH2O:  lock (((aquired))) %s\n", bufferH2O);
 
-  oxygens++;
-  printk("DEBUG:readH2O:  there's %d oxygens %s\n", oxygens, bufferH2O);
+  enqueuedOxygens++;
+  printk("DEBUG:readH2O:  there's %d oxygens %s\n", enqueuedOxygens, bufferH2O);
   try:
   {
     while (enqueuedHydrogens < 2) {
@@ -263,20 +269,29 @@ static ssize_t readH2O(struct file *pFile, char *buf, size_t ucount, loff_t *pFi
       }
       printk("DEBUG:readH2O:  I'm awake %s\n", bufferH2O);
     }
+    while (writtenHydrogens < 2) {
+      printk("DEBUG:readH2O:  Not enough writes. Going to sleep %s\n", bufferH2O);
+      //  The procedure waits for hydrogens to be written to the file
+      if (c_wait(&waitingHydrogen, &mutex)) {
+        printk("<1>read interrupted\n");
+        count = -EINTR;
+        goto finally;
+      }
+      printk("DEBUG:readH2O:  I'm awake %s\n", bufferH2O);
+    }
 
     if (count > size) {
       count = size;
     }
 
-    /* Transfiriendo datos hacia el espacio del usuario */
+    //  Transfering data to user space
     for (k = 0; k < count; k++) {
       if (copy_to_user(buf + k, bufferH2O + out, 1) != 0) {
-        /* el valor de buf es una direccion invalida */
+        //  buf is an invalid adress
         count = -EFAULT;
         goto finally;
       }
-      printk("<1>read byte %c (%d) from %d\n",
-             bufferH2O[out], bufferH2O[out], out);
+      printk("<1>read byte %c (%d) from %d\n", bufferH2O[out], bufferH2O[out], out);
       out = (out + 1) % MAX_SIZE;
       size--;
     }
@@ -304,7 +319,7 @@ static ssize_t writeH2O(struct file *pFile, const char *buf, size_t ucount,
   {
     enqueuedHydrogens++;
     printk("DEBUG:writeH2O: There's %d enqueued hydrogens %s\n", enqueuedHydrogens, buf);
-    while (oxygens < 1) {
+    while (enqueuedOxygens < 1) {
       printk("DEBUG:writeH2O: Not enough oxygens. Going to sleep %s\n", buf);
       // The process waits if there's not enough oxygens to form a molecule
       if (c_wait(&cond, &mutex)) {
@@ -337,9 +352,8 @@ static ssize_t writeH2O(struct file *pFile, const char *buf, size_t ucount,
     enqueuedHydrogens--;
     if (enqueuedHydrogens == 0) {
       printk("DEBUG:writeH2O: Removing  %s\n", buf);
-      oxygens--;
+      enqueuedOxygens--;
       c_broadcast(&waitingMolecule);
-      formingMolecule = FALSE;
     }
   }
   finally:
