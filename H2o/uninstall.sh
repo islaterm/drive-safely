e1aff3ee68c56fe05aff3061ebc72f91f94f6700
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2019-12-22 21:38:07 -0300

    Modified uninstall script v1.0.10.1 The script now removes the module file /dev/h2o after removing the module.

--- H2o/uninstall.sh
+++ H2o/uninstall.sh
@@ -1,7 +1,8 @@
 # Unregisters the h2o module and cleans the output of the compilation.
 #
 # Author: Ignacio Slater Muñoz
-# Version 0.3
+# Version 1.0.10.2
 
 sudo rmmod h2o.ko
+sudo rm /dev/h2o
 make clean
